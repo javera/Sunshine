060e14c509648501e53b80e3f4fc7aa1e587356b
Marek Jaworski <mjaworski1988@gmail.com>
2015-07-14 14:16:28 +0800

    Lesson 1 commit

--- app/src/main/java/com/example/android/sunshine/MainActivityFragment.java
+++ app/src/main/java/com/example/android/sunshine/MainActivityFragment.java
@@ -1,10 +1,16 @@
 package com.example.android.sunshine;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +23,28 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        ArrayAdapter<String> mAdapter;
+
+        // dummy data for the ListView.
+        String[] forecasts = {"Mon - Sunny - 32/28",
+                "Tue - Partly Cloudy - 32/26",
+                "Wed - Rainy - 24/16",
+                "Thu - Thunderstorms - 20/14",
+                "Fri - Rainy - 22/19",
+                "Sat - Foggy - 26/24",
+                "Sun - Partly Cloudy - 29/26"};
+
+        List<String> forecastList = new ArrayList<String>(Arrays.asList(forecasts));
+
+        mAdapter = new ArrayAdapter<String>(getActivity(),
+                R.layout.list_item_forecast,
+                R.id.list_item_forecast_textview,
+                forecastList);
+
+        View fragmentView = inflater.inflate(R.layout.fragment_main, container, false);
+        ListView listview = (ListView) fragmentView.findViewById(R.id.list_view_forecast);
+        listview.setAdapter(mAdapter);
+
+        return fragmentView;
     }
 }
